894ec8ce270244a8556e8b05791a150e4dae88a0
omaradel-65 <01112987264o@gmail.com>
2023-03-04 09:20:54 +0200

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes , Route ,Link} from 'react-router-dom'
 import './App.css';
-
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Details from './components/Details/Details';
+import Details2 from './components/Details/Details2';
+import Details3 from './components/Details/Details3';
+import Details4 from './components/Details/Details4';
+import Details5 from './components/Details/Details5';
+import Details6 from './components/Details/Details6';
+import Details7 from './components/Details/Details7';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+<BrowserRouter>
+<Header/>
+<Routes>
+
+<Route path='/' element={<Home />} />
+<Route path='/Details' element={<Details/>} />
+<Route path='/Details2' element={<Details2/>} />
+<Route path='/Details3' element={<Details3/>} />
+<Route path='/Details4' element={<Details4/>} />
+<Route path='/Details5' element={<Details5/>} />
+<Route path='/Details6' element={<Details6/>} />
+<Route path='/Details7' element={<Details7/>} />
+</Routes>
+</BrowserRouter>
   );
 }
 
